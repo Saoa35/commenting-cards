225870cd3644418be1028869f57af8c9b4d34eb6
Saoa35 <saoamah@gmail.com>
2022-12-20 21:25:27 +0100

    installed react-redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="" alt="Image" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
