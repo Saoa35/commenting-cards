d2412525933ac570eddc64423801d1f36d3cbc84
Saoa35 <saoamah@gmail.com>
2022-12-25 13:53:35 +0100

    created handleSubmit method

--- src/Comments.js
+++ src/Comments.js
@@ -8,9 +8,14 @@ function Comments(props) {
     setTextComment(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(textComment);
+  };
+
   return (
     <div className="card-comments">
-      <form className="comments-item-create">
+      <form onSubmit={handleSubmit} className="comments-item-create">
         <input className="text" value={textComment} onChange={handleInput} />
         <input className="submit" hidden />
       </form>
