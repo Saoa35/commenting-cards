ef671024a83937c233b52d108fab642b08dfad30
Saoa35 <saoamah@gmail.com>
2022-12-25 10:13:08 +0100

    imported useState

--- src/Comments.js
+++ src/Comments.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import SingleComment from "./SingleComment";
 
 function Comments(props) {
+  const [textComment, setTextComment] = useState("");
+
+  const handleInput = (e) => {
+    setTextComment(e.target.value);
+  };
+
   return (
     <div className="card-comments">
       <form className="comments-item-create">
-        <input className="text" />
+        <input className="text" value={textComment} onChange={handleInput} />
         <input className="submit" hidden />
       </form>
       <SingleComment />
