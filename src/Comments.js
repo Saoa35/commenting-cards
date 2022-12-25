cd79fc314a689ddc37ab9d60480d6740a380f41b
Saoa35 <saoamah@gmail.com>
2022-12-25 14:20:41 +0100

    imported commentCreate

--- src/Comments.js
+++ src/Comments.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import SingleComment from "./SingleComment";
+import { commentCreate } from "./redux/actions";
 
 function Comments(props) {
   const [textComment, setTextComment] = useState("");
