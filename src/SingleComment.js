8eeb2f7502cfa28aff970dc1d6590e699a39710c
Saoa35 <saoamah@gmail.com>
2022-12-26 10:14:05 +0100

    imported useState

--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 function SingleComment({ data }) {
+  const [commentText, setCommentText] = useState("");
   const { text, id } = data;
+
   return (
     <form className="comments-item">
       <div className="comments-item-delete">&times;</div>
