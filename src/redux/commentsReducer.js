331c3a6645a746efa862c1bff98fb836ff709c59
Saoa35 <saoamah@gmail.com>
2022-12-30 20:47:37 +0100

    added COMMENT_UPDATE case

--- src/redux/commentsReducer.js
+++ src/redux/commentsReducer.js
@@ -1,4 +1,4 @@
-import { COMMENT_CREATE } from "./types";
+import { COMMENT_CREATE, COMMENT_UPDATE } from "./types";
 
 const initialState = {
   comments: [],
@@ -8,6 +8,9 @@ export const commentsReducer = (state = initialState, action) => {
   switch (action.type) {
     case COMMENT_CREATE:
       return { ...state, comments: [...state.comments, action.data] };
+
+    case COMMENT_UPDATE:
+      return { ...state };
 
     default:
       return state;
