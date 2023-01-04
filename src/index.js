b1f1a1929bb013e8eb3f03c5b9d8514ba2bb4592
Saoa35 <saoamah@gmail.com>
2023-01-04 18:28:32 +0100

    + window.__REDUX_DEVTOOLS_EXTENSION

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
