09f8ddabc2af17b624e6567b1ee727d17bea048f
Saoa35 <saoamah@gmail.com>
2023-01-04 18:25:02 +0100

    imported applyMiddleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { compose, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose());
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
