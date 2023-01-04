23dd865aa19e2c439b86d7eebf78f0d764c6db72
Saoa35 <saoamah@gmail.com>
2023-01-04 18:24:11 +0100

    imported compose

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { compose, createStore } from "redux";
 import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, compose());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
