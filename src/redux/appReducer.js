d3dd2ee2a930cfaa6edc454e1676767e37b18822
Saoa35 <saoamah@gmail.com>
2023-01-21 09:20:49 +0100

    imported  LOADER_DISPLAY_OF & LOADER_DISPLAY_ON

--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -1,3 +1,20 @@
+import { LOADER_DISPLAY_OF, LOADER_DISPLAY_ON } from "./types";
+
 const initialState = {
   loading: false,
+};
+
+export const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOADER_DISPLAY_ON:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOADER_DISPLAY_OF:
+      return {
+        ...state,
+        loading: false,
+      };
+  }
 };
