1b68a4e1ef7ecda73250065bfcd17858a9e9eefe
Saoa35 <saoamah@gmail.com>
2023-01-28 19:22:33 +0100

    created Spin component

--- src/Spin.js
+++ src/Spin.js
@@ -1,1 +1,15 @@
 import Loader from "react-loader-spinner";
+
+const Spin = (props) => {
+  return (
+    <div className="loader-styles">
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    </div>
+  );
+};
