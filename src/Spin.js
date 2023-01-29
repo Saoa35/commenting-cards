5cd75742d530a9f1c702f6e9682fabe3320d2386
Saoa35 <saoamah@gmail.com>
2023-01-29 09:41:57 +0100

    imported useSelector

--- src/Spin.js
+++ src/Spin.js
@@ -1,6 +1,8 @@
 import Loader from "react-loader-spinner";
+import { useSelector } from "react-redux";
 
 const Spin = (props) => {
+  const spinner = useSelector((state) => {});
   return (
     <div className="loader-styles">
       <Loader
